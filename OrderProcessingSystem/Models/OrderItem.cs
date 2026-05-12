24fb506cd90ca34f05e2036b7554e5faebd79846
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-12 09:40:50 +0400

    Final Update

--- OrderProcessingSystem/Models/OrderItem.cs
+++ OrderProcessingSystem/Models/OrderItem.cs
@@ -1,30 +1,41 @@
-﻿
-namespace OrderProcessingSystem.Models
+﻿namespace OrderProcessingSystem.Models
 {
     public class OrderItem
     {
+        // Store product information
         public Product Product { get; set; }
+
+        // Store product quantity
         public int Quantity { get; set; }
 
+        // Calculate subtotal after discount
         public decimal SubTotal
         {
             get
             {
+                // Get discount value
                 decimal discount = Product.CalculateDiscount();
+
+                // Calculate final product price
                 decimal finalPrice = Product.Price - discount;
+
+                // Return subtotal
                 return finalPrice * Quantity;
             }
         }
 
+        // Constructor
         public OrderItem(Product product, int quantity)
         {
             Product = product;
+
             Quantity = quantity;
         }
 
+        // Display order item details
         public override string ToString()
         {
-            return $"{Product.Name} x {Quantity} = {SubTotal} OMR";
+            return Product.Name + " x " + Quantity + " = " + SubTotal + " OMR";
         }
     }
 }
